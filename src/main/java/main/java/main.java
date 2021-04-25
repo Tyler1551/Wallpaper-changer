4294c7f8af2d085beaa6ae480d5fe0690d816061
Tyler1551 <Tylerpower1551@gmail.com>
2021-04-25 17:04:29 +0100

    changes wallpaper, is on a 10 minute timer between api calls

--- src/main/java/main/java/main.java
+++ src/main/java/main/java/main.java
@@ -1,16 +1,95 @@
 package main.java;
 
+import com.sun.jna.*;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.win32.StdCallLibrary;
+import com.sun.jna.win32.W32APIFunctionMapper;
+import com.sun.jna.win32.W32APITypeMapper;
 import org.json.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class main {
-    public static void main(String[] args) throws IOException {
-        StringBuffer weather = fetch_weather();
 
+    public interface SPI extends StdCallLibrary {
+
+        //from MSDN article
+        long SPI_SETDESKWALLPAPER = 20;
+        long SPIF_UPDATEINIFILE = 0x01;
+        long SPIF_SENDWININICHANGE = 0x02;
+
+        SPI INSTANCE = (SPI) Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
+            {
+                put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+                put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+            }
+        });
+
+        void SystemParametersInfo(
+                WinDef.UINT_PTR uiAction,
+                WinDef.UINT_PTR uiParam,
+                String pvParam,
+                WinDef.UINT_PTR fWinIni
+        );
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        StringBuffer weather = fetch_weather();
+        String weather_type = processJson(weather);
+
+        System.out.println(weather_type);
+        String wallpaper_path = "";
+
+        TimerTask update = new TimerTask() {
+            @Override
+            public void run() {
+                ChangeWallpaper(wallpaper_path, weather_type);
+                System.out.println("System updated Wallpaper");
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(update, 0, 600000);
+
+
+    }
+
+    public static void ChangeWallpaper(String wallpaper_path, String weather_type){
+
+        switch (weather_type){
+            case "Clear":
+                wallpaper_path = "C:\\Users\\tyler\\IdeaProjects\\Wallpaper_Change\\Wallpapers\\firewatch_sunny.jpg";
+                break;
+
+            case "Rain":
+                wallpaper_path = "C:\\Users\\tyler\\IdeaProjects\\Wallpaper_Change\\Wallpapers\\firewatch_rain.jpg";
+                break;
+
+            case "Snow":
+                wallpaper_path = "C:\\Users\\tyler\\IdeaProjects\\Wallpaper_Change\\Wallpapers\\firewatch_snow.png";
+                break;
+            case "Clouds":
+                wallpaper_path = "C:\\Users\\tyler\\IdeaProjects\\Wallpaper_Change\\Wallpapers\\cloudy.jpg";
+                break;
+            default:
+                break;
+        }
+
+        SPI.INSTANCE.SystemParametersInfo(
+                new WinDef.UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
+                new WinDef.UINT_PTR(0),
+                wallpaper_path,
+                new WinDef.UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
+    }
+
+    public static String processJson(StringBuffer weather){
         JSONObject obj = new JSONObject(weather.toString());
         JSONArray data = obj.getJSONArray("list");
 
@@ -20,17 +99,13 @@ public class main {
         JSONObject desc = (JSONObject) weather_data.get(0);
         String weather_type = desc.getString("main");
 
-
-        System.out.println(weather_type);
-
-
-
-
+        System.out.println(weather_data);
+        return weather_type;
     }
 
     public static StringBuffer fetch_weather() throws IOException {
-//        'id=2650225' is edinburgh, for future work maybe get current location to choose city
-        URL api = new URL("http://api.openweathermap.org/data/2.5/forecast?id=2650225&appid=04b52862199178582d63b48f1eef68e3");
+//        'id=2650225' and 'id=3333229' is edinburgh, for future work maybe get current location to choose city
+        URL api = new URL("http://api.openweathermap.org/data/2.5/forecast?id=3333229&appid=04b52862199178582d63b48f1eef68e3");
         HttpURLConnection connection = (HttpURLConnection) api.openConnection();
         connection.setRequestMethod("GET");
 
@@ -40,7 +115,7 @@ public class main {
         String inputLine;
         StringBuffer content = new StringBuffer();
         while((inputLine = in.readLine()) != null){
-            content.append(inputLine + '\n');
+            content.append(inputLine).append('\n');
         }
 
         return content;
