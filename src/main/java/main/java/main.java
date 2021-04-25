8c697129bf05e8654dbc21178fcac6343b11fe6b
Tyler1551 <Tylerpower1551@gmail.com>
2021-04-25 13:38:47 +0100

    api call seperates weather description now

--- src/main/java/main/java/main.java
+++ src/main/java/main/java/main.java
@@ -1,40 +1,36 @@
 package main.java;
 
-import com.google.gson.*;
+import org.json.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.Map;
 
 public class main {
     public static void main(String[] args) throws IOException {
         StringBuffer weather = fetch_weather();
-        JsonElement weather_data = new JsonParser().parse(String.valueOf(weather));
-        JsonObject data = weather_data.getAsJsonObject();
 
-        JsonElement data_body = new JsonParser().parse(String.valueOf(data));
-        JsonObject main = data_body.getAsJsonObject();
+        JSONObject obj = new JSONObject(weather.toString());
+        JSONArray data = obj.getJSONArray("list");
 
-        JsonElement list = data.get("list");
+        JSONObject main = data.getJSONObject(1);
+        JSONArray weather_data = main.getJSONArray("weather");
 
-        String data_weather = data.getAsJsonObject("list").getAsString("weather");
+        JSONObject desc = (JSONObject) weather_data.get(0);
+        String weather_type = desc.getString("main");
 
-        Gson gson = new Gson();
 
-        for(Map.Entry<String, JsonElement> entry : data.entrySet()){
-            System.out.println("key = " + entry.getKey() + ", Value = " + entry.getValue());
-            JsonElement val = entry.getValue();
+        System.out.println(weather_type);
 
-        }
 
 
 
     }
 
     public static StringBuffer fetch_weather() throws IOException {
-        URL api = new URL("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=04b52862199178582d63b48f1eef68e3");
+//        'id=2650225' is edinburgh, for future work maybe get current location to choose city
+        URL api = new URL("http://api.openweathermap.org/data/2.5/forecast?id=2650225&appid=04b52862199178582d63b48f1eef68e3");
         HttpURLConnection connection = (HttpURLConnection) api.openConnection();
         connection.setRequestMethod("GET");
 
